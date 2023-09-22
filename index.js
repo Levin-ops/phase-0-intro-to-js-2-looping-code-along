8630477e7f4d1c5408bd408f55481e6e0f2ca002
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-09-22 23:19:50 +0300

    Loopings

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for (let age=30; age<40; age++){
+    console.log(`I'm ${age} years old. Happy Birthday to me!`)
+
+    debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(nameString,eventName) {
+  const messages=[];
+  for (let i = 0; i <nameString.length; i++) {
+      const message= `Thank you, ${nameString[i]}, for the wonderful ${eventName} gift!`
+      messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
